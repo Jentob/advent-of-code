5238860d77b49ee5346f89c58db9275ed05d90e1
Tobias Jensen <tobias@toje.dev>
2025-12-09 17:00:56 +0100

    Solve 2025 day 5

---
+++ src/solutions/2025/5/part1.ts
@@ -0,0 +1,55 @@
+import { getInputData, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputData(2025, 5);
+
+    const gen = readLines(inputFile.stream());
+
+    const ranges: [number, number][] = [];
+    const mergedRanges: [number, number][] = [];
+
+    let sum = 0;
+
+    while (true) {
+        const { value: line }: { value: string } = await gen.next();
+        if (line === "") break;
+        ranges.push(line.split("-").map(Number) as [number, number]);
+    }
+
+    ranges.sort((a, b) => a[0] - b[0]);
+
+    for (const range of ranges) {
+        const lastRange = mergedRanges[mergedRanges.length - 1]!;
+
+        if (mergedRanges.length === 0 || lastRange[1] < range[0]) {
+            mergedRanges.push(range);
+        } else if (lastRange[1] < range[1]) {
+            lastRange[1] = range[1];
+        }
+    }
+
+    for await (const line of gen) {
+        const value = Number(line);
+
+        let min = 0;
+        let max = mergedRanges.length - 1;
+
+        while (min <= max) {
+            const mid = Math.floor((min + max) / 2);
+            const range = mergedRanges[mid]!;
+
+            if (value >= range[0] && value <= range[1]) {
+                sum++;
+                break;
+            } else if (value < range[0]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+    }
+
+    console.log(sum);
+}
+
+main();
---
+++ src/solutions/2025/5/part2.ts
@@ -0,0 +1,38 @@
+import { getInputData, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputData(2025, 5);
+
+    const gen = readLines(inputFile.stream());
+
+    const ranges: [number, number][] = [];
+    const mergedRanges: [number, number][] = [];
+
+    let sum = 0;
+
+    while (true) {
+        const { value: line }: { value: string } = await gen.next();
+        if (line === "") break;
+        ranges.push(line.split("-").map(Number) as [number, number]);
+    }
+
+    ranges.sort((a, b) => a[0] - b[0]);
+
+    for (const range of ranges) {
+        const lastRange = mergedRanges[mergedRanges.length - 1]!;
+
+        if (mergedRanges.length === 0 || lastRange[1] < range[0]) {
+            mergedRanges.push(range);
+        } else if (lastRange[1] < range[1]) {
+            lastRange[1] = range[1];
+        }
+    }
+
+    for (const range of mergedRanges) {
+        sum += range[1] - range[0] + 1;
+    }
+
+    console.log(sum);
+}
+
+main();
