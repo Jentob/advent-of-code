c01744389bd3eef29be5547e0c37d5f231e5732c
Tobias Jensen <tobias@toje.dev>
2025-12-09 13:29:14 +0100

    Solve 2025 day 4

---
+++ src/solutions/2025/4/part1.ts
@@ -0,0 +1,41 @@
+import { getInputData, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputData(2025, 4);
+
+    // biome-ignore format: -
+    const neighbors = [
+        [-1, -1], [ 0, -1], [ 1, -1],
+        [-1,  0],           [ 1,  0],
+        [-1,  1], [ 0,  1], [ 1,  1],
+    ] as const;
+
+    let count = 0;
+
+    const grid: ("@" | ".")[][] = [];
+    let i = 0;
+    for await (const line of readLines(inputFile.stream())) {
+        grid[i] = Array.from(line) as ("@" | ".")[];
+        i++;
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i]!.length; j++) {
+            if (grid[i]![j] === "@") {
+                let neighborCount = 0;
+                for (const [dx, dy] of neighbors) {
+                    if (grid[i + dy]?.[j + dx] === "@") {
+                        neighborCount++;
+                    }
+                }
+                if (neighborCount < 4) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    console.log(count);
+}
+
+main();
---
+++ src/solutions/2025/4/part2.ts
@@ -0,0 +1,47 @@
+import { getInputData, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputData(2025, 4);
+
+    // biome-ignore format: -
+    const neighbors = [
+        [-1, -1], [ 0, -1], [ 1, -1],
+        [-1,  0],           [ 1,  0],
+        [-1,  1], [ 0,  1], [ 1,  1],
+    ] as const;
+
+    let count = 0;
+
+    const grid: ("@" | "." | "x")[][] = [];
+    let i = 0;
+    for await (const line of readLines(inputFile.stream())) {
+        grid[i] = Array.from(line) as ("@" | "." | "x")[];
+        i++;
+    }
+
+    let changed = true;
+    while (changed) {
+        changed = false;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i]!.length; j++) {
+                if (grid[i]![j] === "@") {
+                    let neighborCount = 0;
+                    for (const [dx, dy] of neighbors) {
+                        if (grid[i + dy]?.[j + dx] === "@") {
+                            neighborCount++;
+                        }
+                    }
+                    if (neighborCount < 4) {
+                        count++;
+                        changed = true;
+                        grid[i]![j] = "x";
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(count);
+}
+
+main();
