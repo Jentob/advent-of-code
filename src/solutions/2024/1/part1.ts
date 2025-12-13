25005a726afe1fd4bc92bce93c88197e6b9f1d4c
Tobias Jensen <tobias@toje.dev>
2025-12-13 02:21:21 +0100

    Solve 2024 day 1

---
+++ src/solutions/2024/1/part1.ts
@@ -0,0 +1,25 @@
+import { getInputFile, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputFile(2024, 1);
+
+    let sum = 0;
+
+    const list1: number[] = [];
+    const list2: number[] = [];
+    for await (const line of readLines(inputFile.stream())) {
+        const split = line.split(/\s+/).map(Number) as [number, number];
+        list1.push(split[0]);
+        list2.push(split[1]);
+    }
+    list1.sort((a, b) => a - b);
+    list2.sort((a, b) => a - b);
+
+    for (let i = 0; i < list1.length; i++) {
+        sum += Math.abs(list1[i]! - list2[i]!);
+    }
+
+    console.log(sum);
+}
+
+main();
---
+++ src/solutions/2024/1/part2.ts
@@ -0,0 +1,23 @@
+import { getInputFile, readLines } from "@/utils";
+
+async function main() {
+    const inputFile = await getInputFile(2024, 1);
+
+    let sum = 0;
+
+    const list1: number[] = [];
+    const list2: Map<number, number> = new Map();
+    for await (const line of readLines(inputFile.stream())) {
+        const split = line.split(/\s+/).map(Number) as [number, number];
+        list1.push(split[0]);
+        list2.set(split[1], (list2.get(split[1]) ?? 0) + 1);
+    }
+
+    for (const number of list1) {
+        sum += (list2.get(number) ?? 0) * number;
+    }
+
+    console.log(sum);
+}
+
+main();
